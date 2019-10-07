3d5bfa2b3863e10aa23f02647f7369f41205740e
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-07 10:28:42 -0300

    clean up params

--- inst/scripts/02.temperature.carstm.R
+++ inst/scripts/02.temperature.carstm.R
@@ -7,17 +7,11 @@ p = aegis.temperature::temperature_parameters(
   project_class = "carstm", # defines which parameter set to load
   inputdata_spatial_discretization_planar_km = 1,  # km controls resolution of data prior to modelling to reduce data set and speed up modelling
   inputdata_temporal_discretization_yr = 24/365,  # ie., every 2 weeks .. controls resolution of data prior to modelling to reduce data set and speed up modelling
-  yrs = 1999:2019,
-  spatial_domain = "snowcrab",  # defines spatial area, currenty: "snowcrab" or "SSE"
-  # spatial_domain = "SSE",  # defines spatial area, currenty: "snowcrab" or "SSE"
-  # areal_units_resolution_km = 10, # km dim of lattice ~ 16 hrs
-  # areal_units_resolution_km = 20, # km dim of lattice ~ 1 hr
+  yrs = 1999:2010,
+  spatial_domain = "SSE",  # defines spatial area, currenty: "snowcrab" or "SSE"
   areal_units_resolution_km = 25, # km dim of lattice ~ 1 hr
   areal_units_proj4string_planar_km = projection_proj4string("utm20")  # coord system to use for areal estimation and gridding for carstm
-  # areal_units_proj4string_planar_km = "+proj=omerc +lat_0=44.0 +lonc=-63.0 +gamma=0.0 +k=1 +alpha=325 +x_0=0 +y_0=0 +ellps=WGS84 +units=km"  # oblique mercator, centred on Scotian Shelf rotated by 325 degrees
 )
-
-
 
 
 if (0) {
