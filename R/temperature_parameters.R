d33805f54fed964f40e31f09a46feeca32186159
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-07 22:27:17 -0300

    add simplified parameter call

--- R/temperature_parameters.R
+++ R/temperature_parameters.R
@@ -13,6 +13,26 @@ temperature_parameters = function( p=NULL, project_name=NULL, project_class="def
 
   # ---------------------
 
+  if (project_class =="carstm_auid") {
+    # translate param values from one project to a unified representation
+    # must be first to catch p
+    P = temperature_parameters(
+      project_class = "carstm", # defines which parameter class / set to load
+      project_name = "temperature",
+      spatial_domain = p$spatial_domain,  # defines spatial area, currenty: "snowcrab" or "SSE"
+      areal_units_overlay = p$areal_units_overlay, # currently: "snowcrab_managementareas",  "groundfish_strata" .. additional polygon layers for subsequent analysis for now ..
+      areal_units_resolution_km = p$areal_units_resolution_km, # km dim of lattice ~ 1 hr
+      areal_units_proj4string_planar_km = p$areal_units_proj4string_planar_km,  # coord system to use for areal estimation and gridding for carstm
+      inputdata_spatial_discretization_planar_km = p$inputdata_spatial_discretization_planar_km,  # 1 km .. some thinning .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
+      inputdata_temporal_discretization_yr = p$inputdata_temporal_discretization_yr,  # ie., weekly .. controls resolution of data prior to modelling to reduce data set and speed up modelling
+      auid = p$auid
+    )
+    return(P)
+  }
+
+
+  # ---------------------
+
   # create/update library list
   p$libs = unique( c( p$libs, RLibrary ( "colorspace",  "fields", "geosphere", "lubridate",  "lattice",
     "maps", "mapdata", "maptools", "parallel",  "rgdal", "rgeos",  "sp", "splancs", "GADMTools" ) ) )
@@ -208,8 +228,8 @@ temperature_parameters = function( p=NULL, project_name=NULL, project_class="def
             control.fixed=H$fixed,  # priors for fixed effects, generic is ok
             # control.inla=list(int.strategy="eb") ,# to get empirical Bayes results much faster.
             # control.inla=list( strategy="laplace", cutoff=1e-6, correct=TRUE, correct.verbose=FALSE ),
-            num.threads=4,
-            # blas.num.threads=4,
+            # num.threads=4,
+            blas.num.threads=8,
             verbose=TRUE
           ) ' )
       }
