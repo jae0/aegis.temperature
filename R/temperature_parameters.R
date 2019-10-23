0a437ba1d7ab2840a37fe0a3a2ff7ee67de7af5d
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-23 19:59:04 -0300

    separate stmv and cartsm more fully

--- R/temperature_parameters.R
+++ R/temperature_parameters.R
@@ -13,28 +13,6 @@ temperature_parameters = function( p=NULL, project_name=NULL, project_class="def
 
   # ---------------------
 
-  if (project_class =="carstm_auid") {
-    # translate param values from one project to a unified representation
-    # must be first to catch p
-    P = temperature_parameters(
-      project_class = "carstm", # defines which parameter class / set to load
-      project_name = "temperature",
-      variabletomodel = "t",
-      yrs = p$yrs,
-      spatial_domain = p$spatial_domain,  # defines spatial area, currenty: "snowcrab" or "SSE"
-      areal_units_overlay = p$areal_units_overlay, # currently: "snowcrab_managementareas",  "groundfish_strata" .. additional polygon layers for subsequent analysis for now ..
-      areal_units_resolution_km = p$areal_units_resolution_km, # km dim of lattice ~ 1 hr
-      areal_units_proj4string_planar_km = p$areal_units_proj4string_planar_km,  # coord system to use for areal estimation and gridding for carstm
-      inputdata_spatial_discretization_planar_km = p$inputdata_spatial_discretization_planar_km,  # 1 km .. some thinning .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
-      inputdata_temporal_discretization_yr = p$inputdata_temporal_discretization_yr,  # ie., weekly .. controls resolution of data prior to modelling to reduce data set and speed up modelling
-      auid = p$auid
-    )
-    return(P)
-  }
-
-
-  # ---------------------
-
   # create/update library list
   p$libs = unique( c( p$libs, RLibrary ( "colorspace",  "fields", "geosphere", "lubridate",  "lattice",
     "maps", "mapdata", "maptools", "parallel",  "rgdal", "rgeos",  "sp", "splancs", "GADMTools" ) ) )
@@ -188,74 +166,5 @@ temperature_parameters = function( p=NULL, project_name=NULL, project_class="def
   }
 
 
-
-  if (project_class=="carstm") {
-    p$libs = unique( c( p$libs, project.library ( "carstm" ) ) )
-
-    if ( !exists("project_name", p)) p$project_name = "temperature"
-
-    if ( !exists("areal_units_strata_type", p)) p$areal_units_strata_type = "lattice" # "stmv_lattice" to use ageis fields instead of carstm fields ... note variables are not the same
-
-    if ( p$spatial_domain == "SSE" ) {
-      if ( !exists("areal_units_overlay", p)) p$areal_units_overlay = "groundfish_strata" #.. additional polygon layers for subsequent analysis for now ..
-      if ( !exists("areal_units_resolution_km", p)) p$areal_units_resolution_km = 25 # km dim of lattice ~ 1 hr
-      if ( !exists("areal_units_proj4string_planar_km", p)) p$areal_units_proj4string_planar_km = projection_proj4string("utm20")  # coord system to use for areal estimation and gridding for carstm
-      p$inputdata_spatial_discretization_planar_km = 1  # 1 km .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
-      p$inputdata_temporal_discretization_yr = 1/12  #  24/365,  # ie., monthly .. controls resolution of data prior to modelling to reduce data set and speed up modelling
-    }
-
-    if ( p$spatial_domain == "snowcrab" ) {
-      if ( !exists("areal_units_overlay", p)) p$areal_units_overlay = "snowcrab_managementareas" # currently: "snowcrab_managementareas",  "groundfish_strata" .. additional polygon layers for subsequent analysis for now ..
-      if ( !exists("areal_units_resolution_km", p)) p$areal_units_resolution_km = 25 # km dim of lattice ~ 1 hr
-      if ( !exists("areal_units_proj4string_planar_km", p)) p$areal_units_proj4string_planar_km = projection_proj4string("utm20")  # coord system to use for areal estimation and gridding for carstm
-      # if ( !exists("areal_units_proj4string_planar_km", p)) p$areal_units_proj4string_planar_km = projection_proj4string("omerc_nova_scotia")  # coord system to use for areal estimation and gridding for carstm
-      p$inputdata_spatial_discretization_planar_km = 1  # 1 km .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
-      p$inputdata_temporal_discretization_yr = 1/52  # ie., every 1 weeks .. controls resolution of data prior to modelling to reduce data set and speed up modelling }
-    }
-
-    if ( !exists("carstm_modelengine", p)) p$carstm_modelengine = "inla.default"  # {model engine}.{label to use to store}
-
-    if ( !exists("carstm_modelcall", p)) {
-      if ( grepl("inla", p$carstm_modelengine) ) {
-        p$libs = unique( c( p$libs, project.library ( "INLA" ) ) )
-
-        p$carstm_modelcall = paste('
-          inla(
-            formula = ', p$variabletomodel, ' ~ 1
-              + f(tiyr, model="ar1", hyper=H$ar1 )
-              + f(year, model="ar1", hyper=H$ar1 )
-              + f(zi, model="rw2", scale.model=TRUE, diagonal=1e-6, hyper=H$rw2)
-              + f(strata, model="bym2", graph=sppoly@nb, scale.model=TRUE, constr=TRUE, hyper=H$bym2)
-              + f(iid_error, model="iid", hyper=H$iid),
-            family = "normal",
-            data= M,
-            control.compute=list(dic=TRUE, config=TRUE),
-            control.results=list(return.marginals.random=TRUE, return.marginals.predictor=TRUE ),
-            control.predictor=list(compute=FALSE, link=1 ),
-            control.fixed=H$fixed,  # priors for fixed effects, generic is ok
-            control.inla=list(strategy="gaussian", int.strategy="eb") ,# to get empirical Bayes results much faster.
-            # control.inla=list(int.strategy="eb") ,# to get empirical Bayes results much faster.
-            # control.inla=list( strategy="laplace", cutoff=1e-6, correct=TRUE, correct.verbose=FALSE ),
-            num.threads=4,
-            blas.num.threads=4,
-            verbose=TRUE
-          ) ' )
-      }
-         #     + f(tiyr2, model="seasonal", season.length=10 )
-        # + f(dyear, model="ar1", hyper=H$ar1 )
-
-      if ( grepl("glm", p$carstm_modelengine) ) {
-        p$carstm_modelcall = paste( 'glm( formula = ', p$variabletomodel, ' ~ 1 + StrataID,  family = gaussian(link="log"), data= M[ which(M$tag=="observations"), ], family=gaussian(link="identity")  ) ' )  # for modelengine='glm'
-      }
-      if ( grepl("gam", p$carstm_modelengine) ) {
-        p$libs = unique( c( p$libs, project.library ( "mgcv"  ) ) )
-        p$carstm_modelcall = paste( 'gam( formula = ', p$variabletomodel, ' ~ 1 + StrataID,  family = gaussian(link="log"), data= M[ which(M$tag=="observations"), ], family=gaussian(link="identity")  ) ' ) # for modelengine='gam'
-      }
-    }
-
-    p = carstm_parameters( p=p, DS="basic" )  # fill in anything missing and some checks
-
-    return(p)
-  }
 
 }
--- inst/scripts/02.temperature.carstm.R
+++ inst/scripts/02.temperature.carstm.R
@@ -3,7 +3,8 @@
 
 # construct basic parameter list defining the main characteristics of the study
 # and some plotting parameters (bounding box, projection, bathymetry layout, coastline)
-p = aegis.temperature::temperature_parameters(
+p = aegis.temperature::temperature_carstm(
+  DS = "parameters",
   project_class = "carstm", # defines which parameter set to load
   project_name = "temperature",
   variabletomodel = "temperature",
@@ -94,8 +95,8 @@ if (0) {
 
   # to recreate the underlying data
   sppoly = areal_units( p=p, redo=TRUE )  # this has already been done in aegis.polygons::01 polygons.R .. should nto have to redo
-  M = temperature.db( p=p, DS="aggregated_data", redo=TRUE )  # will redo if not found .. not used here but used for data matching/lookup in other aegis projects that use bathymetry
-  M = temperature.db( p=p, DS="carstm_inputs", redo=TRUE )  # will redo if not found
+  M = temperature_carstm( p=p, DS="aggregated_data", redo=TRUE )  # will redo if not found .. not used here but used for data matching/lookup in other aegis projects that use bathymetry
+  M = temperature_carstm( p=p, DS="carstm_inputs", redo=TRUE )  # will redo if not found
   # to extract fits and predictions
 
   res = carstm_model( p=p, M=M )
