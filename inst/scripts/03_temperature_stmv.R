b3fcd3b6540c622b97b98d248d21bb0259cd001f
Jae S Choi <choi.jae.seok@gmail.com>
2020-12-14 16:33:08 -0400

    "misc"

--- inst/scripts/03_temperature_stmv.R
+++ inst/scripts/03_temperature_stmv.R
@@ -7,50 +7,12 @@ year.start = year.assessment - 30
 nyrs = year.assessment - year.start
 
 
-p = aegis.temperature::temperature_parameters(
-  project_class="stmv",
-  data_root = project.datadirectory( "aegis", "temperature" ),
-  DATA = 'temperature_db( p=p, DS="stmv_inputs" )',
-  spatial_domain = "canada.east", # default
-  inputdata_spatial_discretization_planar_km = 1/4, # 1==p$pres; controls resolution of data prior to modelling (km .. ie 20 linear units smaller than the final discretization pres)
-  inputdata_temporal_discretization_yr = 1/52,  # ie., weekly .. controls resolution of data prior to modelling to reduce data set and speed up modelling
-  additional.data=c("groundfish", "snowcrab", "USSurvey_NEFSC", "lobster"),
-  yrs = 1950:year.assessment,
-  aegis_dimensionality="space-year-season",
-  stmv_variables = list(Y="t"),  # required as fft has no formulae
-  stmv_global_modelengine = "none",  # only marginally useful
-  stmv_local_modelengine = "twostep" ,
-  stmv_local_modelformula_time = formula( paste(
-    't',
-    '~ s( yr, k=', floor(nyrs*0.3), ', bs="ts") + s(cos.w, k=3, bs="ts") + s(sin.w, k=3, bs="ts")  ',
-    '+ s( yr, cos.w, sin.w, k=', floor(nyrs*0.3), ', bs="ts") ',
-    '+ s( log(z), k=3, bs="ts") + s( plon, k=3, bs="ts") + s( plat, k=3, bs="ts")  ',
-    '+ s( log(z), plon, plat, k=6, bs="ts")  '
-    ) ),
+p = aegis.temperature::temperature_parameters( project_class="stmv")
 
-  stmv_twostep_time = "gam",
-  stmv_twostep_space = "fft",  # everything else is too slow ...
-#  stmv_fft_filter="lowpass matern tapered modelled fast_predictions",  # options for fft method: also matern, krige (very slow), lowpass, lowpass_matern, stmv_variogram_resolve_time
-  stmv_fft_filter="lowpass matern tapered modelled exhaustive_predictions",  # options for fft method: also matern, krige (very slow), lowpass, lowpass_matern, stmv_variogram_resolve_time,
-   # exhaustive_predictions required as data density is variable and low
-  stmv_lowpass_nu = 0.5,  # 0.5=exponential, 1=gaussian
-  stmv_lowpass_phi = stmv::matern_distance2phi( distance=0.5, nu=0.5, cor=0.1 ),  # note: p$pres = 0.5
-  stmv_variogram_method = "fft",
-  stmv_autocorrelation_fft_taper = 0.75,  # benchmark from which to taper .. user level control of smoothness
-  stmv_autocorrelation_localrange = 0.1,  # for reporting in stats
-  stmv_autocorrelation_interpolation = c(  0.3, 0.2, 0.1, 0.01 ),  # range finding
-  stmv_local_model_distanceweighted = TRUE,
-  stmv_filter_depth_m = 10, # the depth covariate is input as  depth (m)
-  stmv_rsquared_threshold = 0.01, # lower threshold .. not used if twostep method
-  stmv_distance_statsgrid = 5, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-  stmv_distance_scale = c(  5, 10, 15, 20, 40, 80, 150  ), # km ... approx guess of 95% AC range, the range also determine limits of localrange
-  stmv_distance_interpolation = c( 5, 10, 15, 20, 40, 80, 150  ) , # range of permissible predictions km (i.e 1/2 stats grid to upper limit) .. in this case 5, 10, 20
-  stmv_distance_prediction_limits =c( 2.5, 7.5 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit) .. in this case 5, 10, 20
-  stmv_nmin = 80,  # min number of unit spatial locations req before attempting to model in a localized space .. control no error in local model
-  stmv_nmax = 80*(nyrs/2), # no real upper bound.. just speed / RAM limits  .. can go up to 10 GB / core if too large
-  stmv_tmin = floor( nyrs * 1.25 ),
-  stmv_force_complete_method = "fft"
-  
+if (0){
+  p$stmv_nmin = 80  # min number of unit spatial locations req before attempting to model in a localized space .. control no error in local model
+  p$stmv_nmax = 80*(nyrs/2) # no real upper bound.. just speed / RAM limits  .. can go up to 10 GB / core if too large
+  p$stmv_tmin = floor( nyrs * 1.25 )
 )
 
 
@@ -71,8 +33,8 @@ if (0) {
           cor_0.01 = rep("localhost", interpolate_ncpus)
         ),
         interpolate_predictions = list(
-          c1 = rep("localhost", interpolate_ncpus),  
-          c2 = rep("localhost", interpolate_ncpus),  
+          c1 = rep("localhost", interpolate_ncpus),
+          c2 = rep("localhost", interpolate_ncpus),
           c3 = rep("localhost", interpolate_ncpus),
           c4 = rep("localhost", interpolate_ncpus),
           c5 = rep("localhost", interpolate_ncpus),
