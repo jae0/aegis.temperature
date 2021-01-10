e67b9fe1b30477d166a824626e88da1e0df5d21e
Jae S Choi <choi.jae.seok@gmail.com>
2021-01-10 16:37:02 -0400

    typos

--- R/temperature_parameters.R
+++ R/temperature_parameters.R
@@ -74,7 +74,7 @@ temperature_parameters = function( p=list(), project_name="temperature", project
     )
 
     if ( !exists("carstm_inputadata_model_source", p))  p$carstm_inputadata_model_source = list()
-    if ( !exists("bathymetry", p$carstm_inputadata_model_source ))  p$carstm_inputadata_model_source$bathymetry = "stmv",  # "stmv", "hybrid", "carstm"
+    if ( !exists("bathymetry", p$carstm_inputadata_model_source ))  p$carstm_inputadata_model_source$bathymetry = "stmv"  # "stmv", "hybrid", "carstm"
 
     if ( !exists("carstm_model_call", p)  ) {
       if ( grepl("inla", p$carstm_modelengine) ) {
