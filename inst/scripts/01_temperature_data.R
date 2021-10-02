adf646ab0e8b4472b53665c5c0f8f6a873aea41d
Jae S Choi <choi.jae.seok@gmail.com>
2021-10-02 11:37:36 -0300

    misc

--- inst/scripts/01_temperature_data.R
+++ inst/scripts/01_temperature_data.R
@@ -40,10 +40,10 @@ p = aegis.temperature::temperature_parameters( yrs=1950:year.assessment )  # the
  
   # Extract bottom data from each profile and discretization of space and time resolution to manageable numbers
   # temperature_db( DS="bottom.annual.redo", p=p, yr=1900:year.assessment )
-  temperature_db( DS="bottom.annual.redo", p=p,   yr=p$yrs ) 
+  o = temperature_db( DS="bottom.annual.redo", p=p,   yr=p$yrs ) 
   
   (p$yrs) # check the years to ensure we are selecting the correct years 1950:present
-    temperature_db ( DS="bottom.all.redo", p=p )
+  temperature_db ( DS="bottom.all.redo", p=p )
 
   o = temperature_db ( DS="aggregated_data", p=p, redo=TRUE )
 
