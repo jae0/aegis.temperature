d6b9acff27ea54c2385bca55b3d227b5551942f7
Jae S Choi <choi.jae.seok@gmail.com>
2021-10-18 15:03:50 -0300

    tweaks

--- R/temperature_parameters.R
+++ R/temperature_parameters.R
@@ -106,7 +106,7 @@ temperature_parameters = function( p=list(), project_name="temperature", project
         # generics using "default" carstm models and stmv solutions for spatial effects
         p$carstm_lookup_parameters = list()
         p$carstm_lookup_parameters = parameters_add_without_overwriting( p$carstm_lookup_parameters,
-          bathymetry = bathymetry_parameters( project_class="stmv", spatial_domain=p$spatial_domain, stmv_model_label="default"  )
+          bathymetry = bathymetry_parameters( project_class="stmv"  )
         )
     }
 
