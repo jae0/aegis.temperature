c94eda5916bc97d0580bbaa45bee844dfdb86031
Jae S Choi <choi.jae.seok@gmail.com>
2022-02-07 13:36:21 -0400

    cleanup

--- inst/scripts/03_temperature_carstm_1970_present.R
+++ inst/scripts/03_temperature_carstm_1970_present.R
@@ -56,7 +56,7 @@
     num.threads="4:2",  # adjust for your machine
     mc.cores=2,
     # if problems, try any of: 
-    control.inla = list( strategy='laplace'),
+    # control.inla = list( strategy='laplace'),
     # control.inla = list( strategy='adaptive', int.strategy="eb" ),
     # redo_fit=TRUE, # to start optim from a solution close to the final in 2021 ... 
     redo_fit=TRUE, # to start optim from a solution close to the final in 2021 ... 
@@ -69,45 +69,45 @@
 
 ## --- long run results: 1970-present
 
+  # Deviance Information Criterion (DIC) ...............: 1030247.34
+  # Deviance Information Criterion (DIC, saturated) ....: 4326334.48
+  # Effective number of parameters .....................: 21323.92
+
+  # Watanabe-Akaike information criterion (WAIC) ...: 1031476.32
+  # Effective number of parameters .................: 20380.37
+
+  # Marginal log-Likelihood:  -492527.45 
+  # Posterior summaries for the linear predictor and the fitted values are computed
+  # (Posterior marginals needs also 'control.compute=list(return.marginals.predictor=TRUE)')
+
+  #   --- NOTE: parameter estimates are on link scale and not user scale.
+
+  #   --- NOTE: even if the model fit completes, failure of extraction is possible when NAN or INF are encountered... this means that your model / data probably needs tbe changed.
+
+
+  # Computing summaries and computing from posterior simulations (can be longer than model fitting depending upon no of posterior sims: 'nposteriors' ) ...
+  # Extracting parameter summaries
+
+  # Fixed effects
+  #               mean       sd quant0.025 quant0.5 quant0.975   parameter
+  # (Intercept) 6.19742 0.143409    5.91599    6.197    6.47802 (Intercept)
+
+  # Model may be over parameterized. NAN and Inf values encountered in phis. Try alt parameterizations or smaller number of n or masking negative values
+
+  # Random effects:
+  #                                                   mean         sd quant0.025 quant0.5 quant0.975
+  # SD the Gaussian observations                   1.562947 0.00345120   1.555127 1.563424   1.568302
+  # SD time                                        0.669060 0.00936281   0.654173 0.667805   0.690158
+  # SD cyclic                                      0.830288 0.04820958   0.765649 0.820649   0.946243
+  # SD space                                       3.250345 0.05948477   3.152214 3.243385   3.382347
+  # SD inla.group(z, method = "quantile", n = 11) 11.427465 5.44706444   5.930292 9.810387  26.121735
+  # SD space_time                                  1.457522 0.01163610   1.433461 1.458056   1.479000
+  # Rho for time                                   0.377329 0.02457540   0.321641 0.380658   0.415802
+  # GroupRho for space_time                        0.539559 0.01036008   0.519184 0.539531   0.559783
+
+
+
  
-
-
-
-##############
-
-
-## --- short run results: 1999- present
-# Fitted model saved as: /home/jae/bio.data/aegis/temperature/modelled/1999_present/temperature|canada.east|tesselation|1|none|40|10|none.rdata|carstm_modelled_summary|t|inla|100|7.rdata
-
-# Deviance Information Criterion (DIC) ...............: 546440.39
-# Deviance Information Criterion (DIC, saturated) ....: 2500670.27
-# Effective number of parameters .....................: 10878.08
-
-# Watanabe-Akaike information criterion (WAIC) ...: 547416.00
-# Effective number of parameters .................: 10607.07
-
-
-# Fixed effects
-#             mean       sd           quant0.025 quant0.5   quant0.975
-# (Intercept) 6.16773144 0.0600921552 6.04969833 6.16761527 6.28568136
-
-
-# Random effects:
-#                                                       mean             sd   quant0.025     quant0.5  quant0.975
-# SD the Gaussian observations                    1.31971928  0.00274093815    1.3138489   1.31993557  1.32452319
-# SD cyclic                                       18.4943731    0.584222092   17.5445605   18.4209961  19.8042772
-# SD time                                         23.5569114    0.271565676   23.0005998    23.567039  24.0645223
-# SD space                                        1.43986716   0.0278023228   1.38457729    1.4401821  1.49367044
-# SD inla.group(z, method = "quantile", n = 11)   1.49313565     0.26008593   1.09551099   1.45176975  2.10720371
-# SD space_time                                   1.24155046   0.0132395874   1.21421637   1.24214444  1.26602426
-# Rho for time                                  -0.999747217 8.99040293e-06 -0.999764035 -0.999747555 -0.99972883
-# Phi for space                                  0.995140678  0.00147205219  0.991924236   0.99525121 0.997603221
-# Phi for space_time                             0.999999813 4.50164302e-06  0.999961747  0.999997158 0.999998399
-# GroupRho for space_time                        0.299742681   0.0167560123  0.266076008  0.300014107 0.331725306
-#    --- NOTE: 'SD *' are on link scale and not user scale
-
-
-
       
     # extract results
     if (0) {
