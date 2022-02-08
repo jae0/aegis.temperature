8a166713a69e79ab27f2dc9288189be7471e3978
Jae S Choi <choi.jae.seok@gmail.com>
2022-02-08 10:35:39 -0400

    misc

--- inst/scripts/03_temperature_carstm_1970_present.R
+++ inst/scripts/03_temperature_carstm_1970_present.R
@@ -28,7 +28,8 @@
  
 
     # to recreate the underlying data
-    # NOTE:: this reshapes the boundaries to make things a bit more compact .... will need to alter to get more expansive results
+    # NOTE:: this reshapes the geographic boundaries to make things a bit more compact 
+    # .... will need to alter to get more expansive results
     xydata=temperature_db(p=p, DS="areal_units_input", redo=TRUE)  # redo if inpute data has changed
     
     xydata=temperature_db(p=p, DS="areal_units_input" )  # redo if inpute data has changed
@@ -58,7 +59,7 @@
     mc.cores=2,
     # if problems, try any of: 
     # control.inla = list( strategy='laplace'),
-    # control.inla = list( strategy='adaptive', int.strategy="eb" ),
+    control.inla = list( strategy='adaptive', int.strategy="eb" ),
     # redo_fit=TRUE, # to start optim from a solution close to the final in 2021 ... 
     redo_fit=TRUE, # to start optim from a solution close to the final in 2021 ... 
     # debug = TRUE,
@@ -69,45 +70,44 @@
 
 
 ## --- long run results: 1970-present
+# Deviance Information Criterion (DIC) ...............: 1028957.59
+# Deviance Information Criterion (DIC, saturated) ....: 4345288.12
+# Effective number of parameters .....................: 24526.26
 
-  # Deviance Information Criterion (DIC) ...............: 1030247.34
-  # Deviance Information Criterion (DIC, saturated) ....: 4326334.48
-  # Effective number of parameters .....................: 21323.92
+# Watanabe-Akaike information criterion (WAIC) ...: 1030893.52
+# Effective number of parameters .................: 23671.73
 
-  # Watanabe-Akaike information criterion (WAIC) ...: 1031476.32
-  # Effective number of parameters .................: 20380.37
+# Marginal log-Likelihood:  -493362.38 
+# Posterior summaries for the linear predictor and the fitted values are computed
+# (Posterior marginals needs also 'control.compute=list(return.marginals.predictor=TRUE)')
 
-  # Marginal log-Likelihood:  -492527.45 
-  # Posterior summaries for the linear predictor and the fitted values are computed
-  # (Posterior marginals needs also 'control.compute=list(return.marginals.predictor=TRUE)')
+#    --- NOTE: parameter estimates are on link scale and not user scale.
 
-  #   --- NOTE: parameter estimates are on link scale and not user scale.
-
-  #   --- NOTE: even if the model fit completes, failure of extraction is possible when NAN or INF are encountered... this means that your model / data probably needs tbe changed.
-
-
-  # Computing summaries and computing from posterior simulations (can be longer than model fitting depending upon no of posterior sims: 'nposteriors' ) ...
-  # Extracting parameter summaries
-
-  # Fixed effects
-  #               mean       sd quant0.025 quant0.5 quant0.975   parameter
-  # (Intercept) 6.19742 0.143409    5.91599    6.197    6.47802 (Intercept)
-
-  # Model may be over parameterized. NAN and Inf values encountered in phis. Try alt parameterizations or smaller number of n or masking negative values
-
-  # Random effects:
-  #                                                   mean         sd quant0.025 quant0.5 quant0.975
-  # SD the Gaussian observations                   1.562947 0.00345120   1.555127 1.563424   1.568302
-  # SD time                                        0.669060 0.00936281   0.654173 0.667805   0.690158
-  # SD cyclic                                      0.830288 0.04820958   0.765649 0.820649   0.946243
-  # SD space                                       3.250345 0.05948477   3.152214 3.243385   3.382347
-  # SD inla.group(z, method = "quantile", n = 11) 11.427465 5.44706444   5.930292 9.810387  26.121735
-  # SD space_time                                  1.457522 0.01163610   1.433461 1.458056   1.479000
-  # Rho for time                                   0.377329 0.02457540   0.321641 0.380658   0.415802
-  # GroupRho for space_time                        0.539559 0.01036008   0.519184 0.539531   0.559783
+#    --- NOTE: even if the model fit completes, failure of extraction is possible when NAN or INF are encountered... this means that your model / data probably needs tbe changed.
 
 
+# Computing summaries and computing from posterior simulations (can be longer than model fitting depending upon no of posterior sims: 'nposteriors' ) ...
+# Extracting parameter summaries
 
+# Fixed effects
+#                mean      sd quant0.025 quant0.5 quant0.975   parameter
+# (Intercept) 6.25488 0.12336    6.01278  6.25451    6.49624 (Intercept)
+
+# Model may be over parameterized. NAN and Inf values encountered in phis. Try alt parameterizations or smaller number of n or masking negative values
+
+# Random effects:
+#                                                   mean          sd quant0.025 quant0.5 quant0.975
+# SD the Gaussian observations                  1.533401 2.28628e-03   1.529154 1.533297   1.538112
+# SD time                                       0.591332 1.12385e-02   0.574946 0.589442   0.617457
+# SD cyclic                                     0.513943 1.74034e-02   0.474958 0.516244   0.541037
+# SD space                                      3.564353 5.68494e-02   3.463918 3.559885   3.685789
+# SD inla.group(z, method = "quantile", n = 11) 6.778496 1.60957e+00   4.458754 6.478165  10.697137
+# SD space_time                                 1.556909 1.13744e-02   1.534090 1.557107   1.578724
+# Rho for time                                  0.384418 2.90973e-02   0.317340 0.388903   0.427952
+# GroupRho for space_time                       0.594716 9.08126e-03   0.576733 0.594742   0.612322
+# Phi for space                                      NaN         NaN   0.000000 0.000000        NaN
+# Phi for space_time                            1.000000 9.94031e-08   1.000000 1.000000   1.000000
+ 
  
       
     # extract results
@@ -144,12 +144,27 @@
 
 
 
+  carstm_plotxy( res, vn=c( "res", "random", "time" ), 
+    type="b", ylim=c(0, 10), xlab="Year", ylab="Botton temperature (Celcius)", h=7  )
+
+  carstm_plotxy( res, vn=c( "res", "random", "cyclic" ), 
+    type="b", col="slategray", pch=19, lty=1, lwd=2.5, ylim=c(-1.5, 1.5),
+    xlab="Season", ylab="Botton temperature (Celcius)", h=7  )
+
+  carstm_plotxy( res, vn=c( "res", "random", "inla.group(z, method = \"quantile\", n = 11)" ), 
+    type="b", col="slategray", pch=19, lty=1, lwd=2.5, ylim=c(0, 10) ,
+    xlab="Depth (m)", ylab="Botton temperature (Celcius)", h=7  )
+
+
+
+
+
   map_centre = c( (p$lon0+p$lon1)/2 - 0.5, (p$lat0+p$lat1)/2   )
   map_zoom = 7
 
   # maps of some of the results
-  tmatch="1970"
-  umatch="0.15"
+  tmatch="2021"
+  umatch="0.75"
 
   tmout = carstm_map(  res=res, vn="predictions", tmatch=tmatch, umatch=umatch, 
     sppoly=sppoly,
@@ -183,7 +198,7 @@
 
 
   tmatch="2020"
-  umatch="0.15"
+  umatch="0.75"
 
   tmout = carstm_map(  res=res, vn=c( "random", "space_time", "combined" ), tmatch=tmatch, umatch=umatch, 
     sppoly=sppoly,
@@ -207,7 +222,7 @@
 
   # slow due to use of webshot to save html to png (partial solution until tmap view mode saves directly)
   for (y in res$time ){
-    for ( u in res$cyclic ){
+    for ( u in res$cyclic[6:8] ){
       fn_root = paste( "Bottom temperature",  as.character(y), as.character(u), sep="-" )
       outfilename = file.path( outputdir, paste( gsub(" ", "-", fn_root), "png", sep=".") )
       tmout = carstm_map(  res=res, vn="predictions", tmatch=as.character(y), umatch=as.character(u),
