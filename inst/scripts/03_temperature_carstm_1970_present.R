fef0525694fb0f8f23ef815fee5426c34606accc
Jae S Choi <choi.jae.seok@gmail.com>
2022-02-14 14:56:47 -0400

    cleanup

--- inst/scripts/03_temperature_carstm_1970_present.R
+++ inst/scripts/03_temperature_carstm_1970_present.R
@@ -11,8 +11,10 @@
       project_class="carstm", 
       yrs=1970:year.assessment, 
       carstm_model_label="1970_present", 
-      theta = c( -0.887, 0.678, 1.573, 0.563, -2.321, 6.928, -4.056, -0.747, 21.491, 1.216   ) 
+      theta = c(-0.79, 0.83, 1.35, 0.88, -2.53, 6.89, -4.01, -0.84, 22.22, 1.33 ) 
     ) 
+   
+   
    
     # List of hyperparameters: 
     # theta[0] = [Log precision for the Gaussian observations]
@@ -30,9 +32,9 @@
     # to recreate the underlying data
     # NOTE:: this reshapes the geographic boundaries to make things a bit more compact 
     # .... will need to alter to get more expansive results
-    xydata=temperature_db(p=p, DS="areal_units_input", redo=TRUE)  # redo if inpute data has changed
+    xydata=temperature_db(p=p, DS="areal_units_input", redo=TRUE)  # redo if input data has changed
     
-    xydata=temperature_db(p=p, DS="areal_units_input" )  # redo if inpute data has changed
+    xydata=temperature_db(p=p, DS="areal_units_input" )  # redo if input data has changed
     xydata = xydata[ which(xydata$yr %in% p$yrs), ]
     sppoly = areal_units( p=p, xydata=xydata, redo=TRUE )  # to force create
 
@@ -59,9 +61,8 @@
     mc.cores=2,
     # if problems, try any of: 
     # control.inla = list( strategy='laplace'),
-    control.inla = list( strategy='adaptive', int.strategy="eb" ),
-    # redo_fit=TRUE, # to start optim from a solution close to the final in 2021 ... 
-    redo_fit=TRUE, # to start optim from a solution close to the final in 2021 ... 
+    # control.inla = list( strategy='adaptive', int.strategy="eb" ),
+    redo_fit=TRUE, 
     # debug = TRUE,
     # debug = "random_covariates",
     verbose=TRUE 
@@ -70,35 +71,45 @@
 
 
 ## --- long run results: 1970-present
-# Deviance Information Criterion (DIC) ...............: 1028957.59
-# Deviance Information Criterion (DIC, saturated) ....: 4345288.12
-# Effective number of parameters .....................: 24526.26
 
-# Watanabe-Akaike information criterion (WAIC) ...: 1030893.52
-# Effective number of parameters .................: 23671.73
+# Deviance Information Criterion (DIC) ...............: 1029074.48
+# Deviance Information Criterion (DIC, saturated) ....: 4380433.99
+# Effective number of parameters .....................: 25496.71
 
-# Marginal log-Likelihood:  -493362.38 
+# Watanabe-Akaike information criterion (WAIC) ...: 1031320.56
+# Effective number of parameters .................: 24744.12
+
+# Marginal log-Likelihood:  -493738.48 
+# Posterior summaries for the linear predictor and the fitted values are computed
+# (Posterior marginals needs also 'control.compute=list(return.marginals.predictor=TRUE)')
+
+#    --- NOTE: parameter estimates are on link scale and not user scale.
+
+#    --- NOTE: even if the model fit completes, failure of extraction is possible when NAN or INF are encountered... this means that your model / data probably needs tbe changed.
+
+
+# Computing summaries and computing from posterior simulations (can be longer than model fitting depending upon no of posterior sims: 'nposteriors' ) ...
+# Extracting parameter summaries
+
 # Fixed effects
-#                mean      sd quant0.025 quant0.5 quant0.975   parameter
-# (Intercept) 6.25488 0.12336    6.01278  6.25451    6.49624 (Intercept)
+#                mean       sd quant0.025 quant0.5 quant0.975   parameter
+# (Intercept) 6.12952 0.121981    5.89146  6.12853    6.36863 (Intercept)
 
-# Model may be over parameterized. NAN and Inf values encountered in phis. Try alt parameterizations or smaller number of n or masking negative values
 
 # Random effects:
 #                                                   mean          sd quant0.025 quant0.5 quant0.975
-# SD the Gaussian observations                  1.533401 2.28628e-03   1.529154 1.533297   1.538112
-# SD time                                       0.591332 1.12385e-02   0.574946 0.589442   0.617457
-# SD cyclic                                     0.513943 1.74034e-02   0.474958 0.516244   0.541037
-# SD space                                      3.564353 5.68494e-02   3.463918 3.559885   3.685789
-# SD inla.group(z, method = "quantile", n = 11) 6.778496 1.60957e+00   4.458754 6.478165  10.697137
-# SD space_time                                 1.556909 1.13744e-02   1.534090 1.557107   1.578724
-# Rho for time                                  0.384418 2.90973e-02   0.317340 0.388903   0.427952
-# GroupRho for space_time                       0.594716 9.08126e-03   0.576733 0.594742   0.612322
-# Phi for space                                      NaN         NaN   0.000000 0.000000        NaN
-# Phi for space_time                            1.000000 9.94031e-08   1.000000 1.000000   1.000000
- 
- 
-      
+# SD the Gaussian observations                  1.489636 0.002316616   1.485455 1.489485   1.494507
+# SD time                                       0.654675 0.058398555   0.544248 0.653379   0.773209
+# SD cyclic                                     0.598698 0.080189860   0.459342 0.592056   0.773596
+# SD space                                      3.592843 0.081680576   3.469444 3.580121   3.780965
+# SD inla.group(z, method = "quantile", n = 11) 8.179492 1.064618633   6.264249 8.118804  10.438437
+# SD space_time                                 1.533367 0.014816895   1.508963 1.531631   1.566200
+# Rho for time                                  0.533253 0.088503469   0.339906 0.540554   0.685016
+# GroupRho for space_time                       0.576076 0.012911940   0.546949 0.577741   0.596559
+# Phi for space                                 0.998661 0.000381386   0.997762 0.998715   0.999242
+# Phi for space_time                            1.000000 0.000000000   1.000000 1.000000   1.000000
+
+
     # extract results
     if (0) {
       # very large files .. slow 
@@ -134,14 +145,14 @@
 
 
   carstm_plotxy( res, vn=c( "res", "random", "time" ), 
-    type="b", ylim=c(0, 10), xlab="Year", ylab="Botton temperature (Celcius)", h=7  )
+    type="b", ylim=c(-2, 2), xlab="Year", ylab="Botton temperature (Celcius)", h=7  )
 
   carstm_plotxy( res, vn=c( "res", "random", "cyclic" ), 
     type="b", col="slategray", pch=19, lty=1, lwd=2.5, ylim=c(-1.5, 1.5),
     xlab="Season", ylab="Botton temperature (Celcius)", h=7  )
 
   carstm_plotxy( res, vn=c( "res", "random", "inla.group(z, method = \"quantile\", n = 11)" ), 
-    type="b", col="slategray", pch=19, lty=1, lwd=2.5, ylim=c(0, 10) ,
+    type="b", col="slategray", pch=19, lty=1, lwd=2.5, ylim=c(-2, 2) ,
     xlab="Depth (m)", ylab="Botton temperature (Celcius)", h=7  )
 
 
