e7564059d751dad6d2a417b0ca9de60ac61ce8d8
Jae S Choi <choijaeseok@gmail.com>
2023-05-27 20:28:55 -0300

    default to seasonal vs cyclic

--- R/temperature_parameters.R
+++ R/temperature_parameters.R
@@ -138,7 +138,7 @@ temperature_parameters = function( p=list(), project_name="temperature", project
         p$formula = as.formula( paste(
          p$variabletomodel, ' ~ 1',
           ' + f( time, model="ar1",  hyper=H$ar1 ) ',   
-          ' + f( cyclic, model="rw2", scale.model=TRUE, hyper=H$rw2, cyclic=TRUE, values=cyclic_values )',
+          ' + f( cyclic, model="seasonal", scale.model=TRUE, season.length=10, hyper=H$iid  )',
           ' + f( space, model="bym2", graph=slot(sppoly, "nb"), scale.model=TRUE, hyper=H$bym2  ) ',
           ' + f( inla.group( z, method="quantile", n=11 ), model="rw2", scale.model=TRUE, hyper=H$rw2)',
           ' + f( space_time, model="bym2", graph=slot(sppoly, "nb"), scale.model=TRUE, group=time_space, hyper=H$bym2, control.group=list(model="ar1", hyper=H$ar1_group) ) '
